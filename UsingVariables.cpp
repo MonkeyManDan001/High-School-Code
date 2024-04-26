cbfee3c4f928cea177d7bc3f5a8f81cb703f5e90
MonkeyManDan001 <168158714+MonkeyManDan001@users.noreply.github.com>
2024-04-26 00:24:21 -0400

    Add files via upload

---
+++ UsingVariables.cpp
@@ -0,0 +1,14 @@
+//Using Variables Dan Mine
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	double radius;
+	radius = 10.0;
+	cout << "Radius = " << radius << endl;
+	cout << endl;
+	radius = 4.5;
+	cout << "Radius = " << radius << endl;
+	return 0;
+}
